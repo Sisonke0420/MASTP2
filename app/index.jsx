0edf31234ec3e52bc231510f2c66a8ca36da5a9d
Sisonke0420 <ST10494718@rcconnect.edu.za>
2026-09-09 15:16:52 +0200

    feat: enhance index component and add about component

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
 
 export default class index extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title }> The freshes to ever do it </Text>
+        
+        <Text style={styles.title}> The freshest to ever do it </Text>
+        <Text> This is a React Native app </Text>
       </View>
     );
   }
@@ -13,12 +15,18 @@ export default class index extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 16,
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
+  logo: {
+    width: 100,
+    height: 100,
+  },
   title: {
     fontSize: 20,
     fontWeight: "bold",
-  }, 
+  },
+
 });
